b4cf07e0c773cbedd03c8606110aa337df307d53
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-10-14 04:33:11 +0330

    chore(calculation): dark_queries and bunous rate
    calculation dark_queries and bunous rate

--- main.py
+++ main.py
@@ -4,38 +4,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-TABLE_NAME = "divar_query"
-FILTER = "action"
 
 if __name__ == "__main__":
     db = DatabaseHandler("divar.db")
 
+    load_post_page_df = db.summons('load_post_page_action') 
+    click_post_df = db.summons("click_post_action")
 
-    load_post_page_df = db.summons(TABLE_NAME, colfil=FILTER,  where=["load_post_page"]) 
-    load_post_page_df.drop(columns=["post_index_in_post_list", "post_token"], inplace=True)
 
-    click_post_df = db.summons(TABLE_NAME, colfil=FILTER, where=["click_post"])
-    click_post_df.drop(columns=["post_page_offset", "tokens"], inplace=True)
+    # Query Percent (less than 10 results)
+    dark_queries = load_post_page_df[load_post_page_df.post_page_offset < 10]
+    dark_query_count = dark_queries.source_event_id.nunique()
 
-    load_post_page_duplicates = load_post_page_df.duplicated().sum()
-    click_post_duplicates = click_post_df.duplicated().sum()
+    total_query_count = load_post_page_df.source_event_id.nunique()
 
-    tables = ['load_post_page_df', 'click_post_df']
-    duplicates = [load_post_page_duplicates, click_post_duplicates]
-    unique_records = [len(load_post_page_df) - load_post_page_duplicates, len(click_post_df) - click_post_duplicates]
+    dark_query_percent = (dark_query_count / total_query_count) * 100
+    print(f"Dark Query Percent (less than 10 results): {dark_query_percent:.2f}%")
 
-    plt.figure(figsize=(15, 4))
-    bar_width = 0.4
-    index = range(len(tables))
 
-    plt.bar(index, unique_records, bar_width, label="Unique Records", color='blue')
-    plt.bar([i + bar_width for i in index], duplicates, bar_width, label="Duplicate Records", color='red')
+    # Query Percent (Click not user)
+    load_queries = load_post_page_df["source_event_id"].unique()
+    clicked_queries = click_post_df["source_event_id"].unique()
 
-    # Labeling the chart
-    plt.xlabel("Tables")
-    plt.ylabel("Record Count")
-    plt.title("Duplicate vs Unique Records in Tables")
-    plt.xticks([i + bar_width/2 for i in index], tables)
-    plt.legend()
+    bounced_queries = set(load_queries) - set(clicked_queries)
+    bounced_coount = len(bounced_queries)
+    total_load_coount = len(load_queries)
 
-    plt.savefig("./image/barplot.png")
+    bounce_rate = (bounced_coount / total_load_coount) * 100 if total_load_coount > 0 else 0
+    print(f"Bounce Rate: {bounce_rate:.2f}%")
+    
+    
+
+
+
+
